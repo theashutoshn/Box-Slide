701c835b225dcba2d28f19d0338437c69bf4243b
Ashutosh <ashunandanwar98@gmail.com>
2024-07-23 00:34:12 +0530

    Level 4 and Level 5 Alignment Fixed

---
+++ Assets/LevelCameraDisable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCameraDisable : MonoBehaviour
+{
+    
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
